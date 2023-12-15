8b0cf389d2f0f6224b876b3b29dcdef78ac2f3f7
SANGDOLEE <xkdxh625@naver.com>
2023-12-16 02:18:32 +0900

    scene 화면 만들어놨는데 아직 제대로 ok했을떄 화면전환이 안되는중

---
+++ RPE/enterView().swift
@@ -0,0 +1,18 @@
+//
+//  enterView().swift
+//  RPE
+//
+//  Created by 이상도 on 12/16/23.
+//
+
+import SwiftUI
+
+struct enterView__: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    enterView__()
+}
