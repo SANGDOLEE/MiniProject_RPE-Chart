fa080d0ccf23fc65e3d47f0ab49bff52349a64a6
SANGDOLEE <xkdxh625@naver.com>
2024-09-17 16:50:43 +0900

    Chore: Delete IsTextViewModel

--- RPE/Source/View/MyRecordView/MyRecordView.swift
+++ RPE/Source/View/MyRecordView/MyRecordView.swift
@@ -117,9 +117,6 @@ struct MyRecordView: View {
                 }
             )
         }
-        .onAppear {
-            // `@AppStorage`에서 자동으로 값을 읽어오므로 이 코드 필요 없음
-        }
         .onTapGesture {
             hideKeyboard()
         }
