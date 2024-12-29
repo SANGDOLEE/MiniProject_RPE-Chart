19578a5ad23c39212f0ec673d5e5f1a7c73e6819
SANGDOLEE <xkdxh625@naver.com>
2024-12-29 18:03:01 +0900

    Feat: Points View 생성

--- RPE/Source/View/Setting/SettingView.swift
+++ RPE/Source/View/Setting/SettingView.swift
@@ -6,74 +6,200 @@ struct SettingView: View {
     @AppStorage("isText") private var unitOfWeight: Bool = false
     
     @State private var showAlert = false
-//    @AppStorage("isDarkModeEnabled") private var isDarkModeEnabled: Bool = false
+    //    @AppStorage("isDarkModeEnabled") private var isDarkModeEnabled: Bool = false
     @Binding var isMainTabbarVisible: Bool
     
     var body: some View {
         NavigationStack {
             ZStack {
-                VStack(spacing: 30) {
-                    // 상단 타이틀
-                    HStack {
-                        Text("Profile")
-                            .font(.setPretendard(weight: .bold, size: 34))
-                            .foregroundStyle(.white)
-                        Spacer()
-                        Image("gear")
-                    }
-                    
-                    VStack(spacing: 12) {
-                        Image(systemName: "person.crop.circle")
-                            .resizable()
-                            .frame(width: 136, height: 136)
+                ScrollView {
+                    VStack(spacing: 30) {
+                        // 상단 타이틀
+                        HStack {
+                            Text("Profile")
+                                .font(.setPretendard(weight: .bold, size: 34))
+                                .foregroundStyle(.white)
+                            Spacer()
+                            Image("gear")
+                        }
                         
-                        Text("Nickname")
-                            .font(.setPretendard(weight: .semiBold, size: 18))
-                            .foregroundStyle(.white)
-                            .kerning(2)
+                        VStack(spacing: 10) {
+                            Image(systemName: "person.crop.circle")
+                                .resizable()
+                                .frame(width: 136, height: 136)
+                            
+                            Text("Hello, Lifter")
+                                .font(.setPretendard(weight: .semiBold, size: 18))
+                                .foregroundStyle(.white)
+                                .kerning(2)
+                            
+                            Text("Male 77.6+")
+                                .font(.setPretendard(weight: .regular, size: 14))
+                                .foregroundStyle(.my1DA4E7)
+                                .kerning(1)
+                        }
                         
-                        Text("Male 77.6+")
-                            .font(.setPretendard(weight: .regular, size: 14))
-                            .foregroundStyle(.my1DA4E7)
-                            .kerning(1)
-                    }
-                    
-                    // MARK: - Update
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("UPDATE")
-                            .font(.setPretendard(weight: .regular, size: 16))
-                            .foregroundStyle(.gray)
-                            .padding(.leading, 8)
+                        HStack(spacing: 10) {
+                            VStack(spacing: 0) {
+                                Image(systemName: "bolt.fill")
+                                    .padding()
+                                    .background(.my181A32)
+                                    .foregroundStyle(.red)
+                                    .clipShape(Circle())
+                                    .padding(.vertical)
+                                
+                                Text("485")
+                                    .font(.setPretendard(weight: .bold, size: 24))
+                                    .foregroundStyle(.white)
+                                    .kerning(0.6)
+                                    .padding(.bottom, 4)
+                                
+                                Text("Total")
+                                    .font(.setPretendard(weight: .regular, size: 12))
+                                    .foregroundStyle(.myB9B9B9)
+                                    .kerning(0.6)
+                                    .padding(.bottom)
+                            }
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 166)
+                            .background(.myBackBoxcolor)
+                            .cornerRadius(12)
+                            
+                            VStack(spacing: 0) {
+                                Image(systemName: "atom")
+                                    .padding()
+                                    .background(.my181A32)
+                                    .foregroundStyle(.yellow)
+                                    .clipShape(Circle())
+                                    .padding(.vertical)
+                                
+                                Text("355.5")
+                                    .font(.setPretendard(weight: .bold, size: 24))
+                                    .foregroundStyle(.white)
+                                    .kerning(0.6)
+                                    .padding(.bottom, 4)
+                                
+                                Text("Dots")
+                                    .font(.setPretendard(weight: .regular, size: 12))
+                                    .foregroundStyle(.myB9B9B9)
+                                    .kerning(0.6)
+                                    .padding(.bottom)
+                                
+                            }
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 166)
+                            .background(.myBackBoxcolor)
+                            .cornerRadius(12)
+                            
+                            VStack(spacing: 0) {
+                                Image(systemName: "globe")
+                                    .padding()
+                                    .background(.my181A32)
+                                    .foregroundStyle(.blue)
+                                    .clipShape(Circle())
+                                    .padding(.vertical)
+                                
+                                Text("360.5")
+                                    .font(.setPretendard(weight: .bold, size: 24))
+                                    .foregroundStyle(.white)
+                                    .kerning(0.6)
+                                    .padding(.bottom, 4)
+                                
+                                Text("Wilks")
+                                    .font(.setPretendard(weight: .regular, size: 12))
+                                    .foregroundStyle(.myB9B9B9)
+                                    .kerning(0.6)
+                                    .padding(.bottom)
+                                
+                            }
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 166)
+                            .background(.myBackBoxcolor)
+                            .cornerRadius(12)
+                            
+                        }
                         
-                        // "My BigThree" 버튼
-                        NavigationLink(destination: UpdateRecordView(viewModel: BigThreeViewModel(),isMainTabbarVisible: $isMainTabbarVisible)) {
+                        // MARK: - Update
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("UPDATE")
+                                .font(.setPretendard(weight: .regular, size: 16))
+                                .foregroundStyle(.gray)
+                                .padding(.leading, 8)
+                            
+                            // "My BigThree" 버튼
+                            NavigationLink(destination: UpdateRecordView(viewModel: BigThreeViewModel(),isMainTabbarVisible: $isMainTabbarVisible)) {
+                                HStack {
+                                    Text("My BigThree")
+                                        .font(.setPretendard(weight: .regular, size: 16))
+                                        .foregroundStyle(.white)
+                                    Spacer()
+                                    Image(systemName: "chevron.right")
+                                        .font(.footnote)
+                                        .bold()
+                                        .foregroundColor(Color(hex: "555556"))
+                                }
+                                .padding()
+                                .background(.myBackBoxcolor)
+                                .cornerRadius(8)
+                            }
+                        }
+                        
+                        // MARK: - Configuration
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("CONFIGURATION")
+                                .font(.setPretendard(weight: .regular, size: 16))
+                                .foregroundStyle(.gray)
+                                .padding(.leading, 8)
+                            
+                            // "Weight Unit Conversion" 버튼
+                            NavigationLink(destination: WeightUnitView(isMainTabbarVisible: $isMainTabbarVisible)) {
+                                HStack {
+                                    Text("Weight Unit Conversion")
+                                        .font(.setPretendard(weight: .regular, size: 16))
+                                        .foregroundStyle(.white)
+                                    Spacer()
+                                    Image(systemName: "chevron.right")
+                                        .font(.footnote)
+                                        .bold()
+                                        .foregroundColor(Color(hex: "555556"))
+                                }
+                                .padding()
+                                .background(Color.myBackBoxcolor)
+                                .cornerRadius(8)
+                            }
+                            
+                            //                    HStack {
+                            //                        Text("Display Mode")
+                            //                        Spacer()
+                            //                        Toggle("Auto Switch Mode", isOn: $isDarkModeEnabled)
+                            //                            .labelsHidden()
+                            //                            .onChange(of: isDarkModeEnabled) { value in
+                            //                                if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+                            //                                    windowScene.windows.first?.overrideUserInterfaceStyle = value ? .dark : .light
+                            //                                }
+                            //                            }
+                            //                    }
+                        }
+                        
+                        // MARK: - 기타 정보 영역
+                        VStack(alignment: .leading, spacing: 3) {
+                            // 앱 버전 정보
                             HStack {
-                                Text("My BigThree")
+                                Text("My RPE version")
                                     .font(.setPretendard(weight: .regular, size: 16))
                                     .foregroundStyle(.white)
                                 Spacer()
-                                Image(systemName: "chevron.right")
-                                    .font(.footnote)
-                                    .bold()
-                                    .foregroundColor(Color(hex: "555556"))
+                                Text(appVersion)
+                                    .font(.setPretendard(weight: .regular, size: 16))
+                                    .foregroundStyle(.white)
                             }
                             .padding()
-                            .background(.myBackBoxcolor)
+                            .background(Color.myBackBoxcolor)
                             .cornerRadius(8)
-                        }
-                    }
-                    
-                    // MARK: - Configuration
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("CONFIGURATION")
-                            .font(.setPretendard(weight: .regular, size: 16))
-                            .foregroundStyle(.gray)
-                            .padding(.leading, 8)
-                        
-                        // "Weight Unit Conversion" 버튼
-                        NavigationLink(destination: WeightUnitView(isMainTabbarVisible: $isMainTabbarVisible)) {
+                            
+                            // 앱 리뷰 남기기
                             HStack {
-                                Text("Weight Unit Conversion")
+                                Text("App reviews")
                                     .font(.setPretendard(weight: .regular, size: 16))
                                     .foregroundStyle(.white)
                                 Spacer()
@@ -85,84 +211,40 @@ struct SettingView: View {
                             .padding()
                             .background(Color.myBackBoxcolor)
                             .cornerRadius(8)
-                        }
-                        
-                        //                    HStack {
-                        //                        Text("Display Mode")
-                        //                        Spacer()
-                        //                        Toggle("Auto Switch Mode", isOn: $isDarkModeEnabled)
-                        //                            .labelsHidden()
-                        //                            .onChange(of: isDarkModeEnabled) { value in
-                        //                                if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-                        //                                    windowScene.windows.first?.overrideUserInterfaceStyle = value ? .dark : .light
-                        //                                }
-                        //                            }
-                        //                    }
-                    }
-                    
-                    // MARK: - 기타 정보 영역
-                    VStack(alignment: .leading, spacing: 3) {
-                        // 앱 버전 정보
-                        HStack {
-                            Text("My RPE version")
-                                .font(.setPretendard(weight: .regular, size: 16))
-                                .foregroundStyle(.white)
-                            Spacer()
-                            Text(appVersion)
-                                .font(.setPretendard(weight: .regular, size: 16))
-                                .foregroundStyle(.white)
-                        }
-                        .padding()
-                        .background(Color.myBackBoxcolor)
-                        .cornerRadius(8)
-                        
-                        // 앱 리뷰 남기기
-                        HStack {
-                            Text("App reviews")
-                                .font(.setPretendard(weight: .regular, size: 16))
-                                .foregroundStyle(.white)
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .font(.footnote)
-                                .bold()
-                                .foregroundColor(Color(hex: "555556"))
-                        }
-                        .padding()
-                        .background(Color.myBackBoxcolor)
-                        .cornerRadius(8)
-                        .onTapGesture {
-                            if let scene = UIApplication.shared.connectedScenes
-                                .first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
-                                SKStoreReviewController.requestReview(in: scene)
+                            .onTapGesture {
+                                if let scene = UIApplication.shared.connectedScenes
+                                    .first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+                                    SKStoreReviewController.requestReview(in: scene)
+                                }
+                            }
+                            
+                            // 의견 및 피드백 남기기
+                            HStack {
+                                Text("Opinion and feedback")
+                                    .font(.setPretendard(weight: .regular, size: 16))
+                                    .foregroundStyle(.white)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .font(.footnote)
+                                    .bold()
+                                    .foregroundColor(Color(hex: "555556"))
+                            }
+                            .padding()
+                            .background(Color.myBackBoxcolor)
+                            .cornerRadius(8)
+                            .onTapGesture {
+                                let urlString = "https://apps.apple.com/app/id6475646908?action=write-review"
+                                if let url = URL(string: urlString) {
+                                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                                }
                             }
                         }
-                        
-                        // 의견 및 피드백 남기기
-                        HStack {
-                            Text("Opinion and feedback")
-                                .font(.setPretendard(weight: .regular, size: 16))
-                                .foregroundStyle(.white)
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .font(.footnote)
-                                .bold()
-                                .foregroundColor(Color(hex: "555556"))
-                        }
-                        .padding()
-                        .background(Color.myBackBoxcolor)
-                        .cornerRadius(8)
-                        .onTapGesture {
-                            let urlString = "https://apps.apple.com/app/id6475646908?action=write-review"
-                            if let url = URL(string: urlString) {
-                                UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                            }
-                        }
+                        .padding(.top)
                     }
-                    .padding(.top)
-                    
-                    Spacer()
+                    .padding()
+                    .padding(.bottom, 12)
                 }
-                .padding()
+                .padding(.bottom, 68)
             }
             .applyGradientBackground()
         }
