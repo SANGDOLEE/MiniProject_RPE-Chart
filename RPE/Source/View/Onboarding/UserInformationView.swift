aab4b96584d25f31605963e1eb588d3d7c9257dd
SANGDOLEE <xkdxh625@naver.com>
2024-12-29 16:38:53 +0900

    Feat: Onboarding BodyWeight View

--- RPE/Source/View/Onboarding/UserInformationView.swift
+++ RPE/Source/View/Onboarding/UserInformationView.swift
@@ -1,21 +1,36 @@
+//
+//  UserInformationView.swift
+//  RPE
+//
+//  Created by 이상도 on 12/29/24.
+//
+
 
 import SwiftUI
 
 // MARK: - 앱의 첫 사용시에만 등장하는 뷰
-struct OnboardingView: View {
+struct UserInformationView: View {
     
-    @State private var navigationRouter = NavigationRouter()
+    @Environment(NavigationRouter.self) var navigationRouter
     @StateObject private var viewModel = BigThreeViewModel()
     @State private var showAlert = false
     @Binding var isPresented: Bool
     
+    @State private var isSelectedGender = "MALE"
+    
     var body: some View {
-        NavigationStack(path: $navigationRouter.path) {
             VStack {
                 HStack {
+                    Button {
+                        navigationRouter.pop()
+                    } label: {
+                        Image(systemName: "chevron.left")
+                            .frame(width: 24, height: 24)
+                            .tint(.white)
+                    }
                     Spacer()
                     Button {
-                        isPresented = false 
+                        isPresented = false
                     } label: {
                         Text("SKIP")
                             .font(.setPretendard(weight: .semiBold, size: 18))
@@ -27,7 +42,7 @@ struct OnboardingView: View {
                     Text("Fill Out Your Information")
                         .font(.setPretendard(weight: .semiBold, size: 26))
                         .foregroundStyle(.white)
-                    Text("This is used to calculate\nyour Big Three total")
+                    Text("For an instant\ncalculation of your Points")
                         .font(.setPretendard(weight: .medium, size: 18))
                         .foregroundStyle(.white)
                         .multilineTextAlignment(.center)
@@ -37,44 +52,48 @@ struct OnboardingView: View {
                 VStack(spacing: 22) {
                     VStack {
                         HStack {
-                            Text("Squat")
+                            Text("Gender")
                                 .font(.setPretendard(weight: .bold, size: 18))
                                 .foregroundStyle(.myAccentcolor)
                             Spacer()
                         }
-                        HStack {
-                            ZStack {
-                                TextField("Enter Weight", text: $viewModel.squatValue)
-                                    .font(.setPretendard(weight: .regular, size: 18))
-                                    .multilineTextAlignment(.leading)
+                        HStack(spacing: 16) {
+                            Button {
+                                isSelectedGender = "MALE"
+                            } label: {
+                                Text("MALE")
+                                    .frame(maxWidth: .infinity)
                                     .frame(height: 44)
-                                    .padding(.horizontal)
-                                    .background(.white)
+                                    .font(.setPretendard(weight: isSelectedGender == "MALE" ? .bold : .regular, size: 18))
+                                    .foregroundStyle(isSelectedGender == "MALE" ? .black : .white)
+                                    .background(isSelectedGender == "MALE" ? .myAccentcolor : .myB9B9B9)
                                     .cornerRadius(12)
-                                    .keyboardType(.decimalPad)
-                                    .onChange(of: viewModel.squatValue) { oldValue, newValue in
-                                        viewModel.squatValue = newValue.prefix(5).filter { "0123456789.".contains($0) }
-                                    }
-                                HStack {
-                                    Spacer()
-                                    XmarkButton(text: $viewModel.squatValue)
-                                }
                             }
-                            Spacer()
+                            
+                            Button {
+                                isSelectedGender = "FEMALE"
+                            } label: {
+                                Text("FEMALE")
+                                    .frame(maxWidth: .infinity)
+                                    .frame(height: 44)
+                                    .font(.setPretendard(weight: isSelectedGender == "FEMALE" ? .bold : .regular, size: 18))
+                                    .foregroundStyle(isSelectedGender == "FEMALE" ? .black : .white)
+                                    .background(isSelectedGender == "FEMALE" ? .myAccentcolor : .myB9B9B9)
+                                    .cornerRadius(12)
+                            }
                         }
                     }
-                    .frame(maxWidth: .infinity)
                     
                     VStack {
-                        HStack {
-                            Text("BenchPress")
+                        HStack(spacing: 2) {
+                            Text("BodyWeight(kgs)")
                                 .font(.setPretendard(weight: .bold, size: 18))
                                 .foregroundStyle(.myAccentcolor)
                             Spacer()
                         }
                         HStack {
                             ZStack {
-                                TextField("Enter Weight", text: $viewModel.benchValue)
+                                TextField("BodyWeight", text: $viewModel.benchValue)
                                     .multilineTextAlignment(.leading)
                                     .frame(height: 44)
                                     .padding(.horizontal)
@@ -92,34 +111,6 @@ struct OnboardingView: View {
                             Spacer()
                         }
                     }
-                    
-                    VStack {
-                        HStack {
-                            Text("Deadlift")
-                                .font(.setPretendard(weight: .bold, size: 18))
-                                .foregroundStyle(.myAccentcolor)
-                            Spacer()
-                        }
-                        HStack {
-                            ZStack {
-                                TextField("Enter Weight", text: $viewModel.deadValue)
-                                    .multilineTextAlignment(.leading)
-                                    .frame(height: 44)
-                                    .padding(.horizontal)
-                                    .background(.white)
-                                    .cornerRadius(12)
-                                    .keyboardType(.decimalPad)
-                                    .onChange(of: viewModel.deadValue) { oldValue, newValue in
-                                        viewModel.deadValue = newValue.prefix(5).filter { "0123456789.".contains($0) }
-                                    }
-                                HStack {
-                                    Spacer()
-                                    XmarkButton(text: $viewModel.deadValue)
-                                }
-                            }
-                            Spacer()
-                        }
-                    }
                     Spacer()
                     
                     Button {
@@ -129,9 +120,7 @@ struct OnboardingView: View {
                         } else {
                             viewModel.saveData()
                             UIApplication.shared.closeKeyboard()
-                            //                        isPresented = false
-                            navigationRouter.push(to: .onboardingUserInformation)
-                            
+                            isPresented = false
                         }
                     } label: {
                         Text("COMPLETE")
@@ -153,21 +142,17 @@ struct OnboardingView: View {
                 }
                 .padding(.top, 36)
             }
-            .ignoresSafeArea(.keyboard, edges: .all)
             .padding()
             .applyGradientBackground()
+        
+//        .ignoresSafeArea(.keyboard, edges: .bottom)
             .onTapGesture {
                 UIApplication.shared.closeKeyboard()
             }
-            .navigationDestination(for: PathType.self) { pathType in
-                            pathType.NavigatingView()
-            }
-        }
-        .environment(navigationRouter)
     }
 }
 
 #Preview {
-    OnboardingView(isPresented: .constant(true))
+    UserInformationView(isPresented: .constant(true))
         .environment(NavigationRouter())
 }
